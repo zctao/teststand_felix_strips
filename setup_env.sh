3d447878aba4a4c01737982b9050182df2348936
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-03-13 17:07:16 -0700

    Initial commit

---
+++ setup_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#GCC
+lcgRelease=${1:-0}
+
+if [[ "$lcgRelease" == 0 ]]; then
+  #setupGCC9
+  #source scl_source enable devtoolset-9; export CC=/usr/bin/gcc; export CXX=/usr/bin/g++;
+  #setupGCC11
+  source scl_source enable devtoolset-11; export CC=/usr/bin/gcc; export CXX=/usr/bin/g++;
+else
+  #source /cvmfs/sft.cern.ch/lcg/contrib/gcc/9/x86_64-centos7/setup.sh
+  source /cvmfs/sft.cern.ch/lcg/contrib/gcc/11/x86_64-centos7/setup.sh
+fi
+
+# Working directory
+export MyWorkDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Taken from https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
