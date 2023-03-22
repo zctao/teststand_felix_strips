d4fa288fcdc612c9f2be110d54ea218af144ee32
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-03-22 16:51:40 -0700

    chipType as name

--- scripts/make_emulator_configs.sh
+++ scripts/make_emulator_configs.sh
@@ -2,8 +2,8 @@
 chipType=${1:-Star} # Star, Star_vH0A0, Star_vH0A1, Star_vH1A1
 
 # Single chip
-$YARRDIR/bin/createConfig -t ${chipType} -s SingleChip -o configs/yarr/emuConfig -n emuStar
+$YARRDIR/bin/createConfig -t ${chipType} -s SingleChip -o configs/yarr/emuConfig -n ${chipType}
 # Long strip stave
-$YARRDIR/bin/createConfig -t ${chipType} -s StripLSStave -o configs/yarr/emuConfig -n emuStar
+$YARRDIR/bin/createConfig -t ${chipType} -s StripLSStave -o configs/yarr/emuConfig -n ${chipType}
 # Petal
-$YARRDIR/bin/createConfig -t ${chipType} -s StripPetal -o configs/yarr/emuConfig -n emuStar
+$YARRDIR/bin/createConfig -t ${chipType} -s StripPetal -o configs/yarr/emuConfig -n ${chipType}
