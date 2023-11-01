102a20a9abc1280ab353865fca27939bcf0f37df
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2023-11-01 00:34:17 -0700

    Update setup_yarr.sh

--- scripts/setup_yarr.sh
+++ scripts/setup_yarr.sh
@@ -16,9 +16,10 @@ function compile_yarr {
   # gcc 11 from LCG release
   source ${TSWorkDIR}/scripts/setup_gcc.sh 1
 
-  cmake3 -S $yarrDir -B $yarrDir/build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=cmake/linux-gcc -DYARR_CONTROLLERS_TO_BUILD="Spec;Emu;StarEmu;Fei4Emu;Rd53aEmu;NetioHW;FelixClient;ItsdaqFW" -DYARR_FRONT_ENDS_TO_BUILD=all -DBUILD_TESTS=on
+  # build only what is necessary
+  cmake3 -S $yarrDir -B $yarrDir/build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=$yarrDir/cmake/linux-gcc -DYARR_CONTROLLERS_TO_BUILD="Spec;StarEmu;FelixClient" -DYARR_FRONT_ENDS_TO_BUILD=all -DBUILD_TESTS=on
   # or build all
-  #cmake3 -S $yarrDir -B $yarrDir/build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=cmake/linux-gcc -DYARR_CONTROLLERS_TO_BUILD=all -DYARR_FRONT_ENDS_TO_BUILD=all -DBUILD_TESTS=on
+  #cmake3 -S $yarrDir -B $yarrDir/build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=$yarrDir/cmake/linux-gcc -DYARR_CONTROLLERS_TO_BUILD="Spec;Emu;StarEmu;Fei4Emu;NetioHW;FelixClient;ItsdaqFW" -DYARR_FRONT_ENDS_TO_BUILD=all -DBUILD_TESTS=on
 
   cmake3 --build $yarrDir/build -j16
   cmake3 --install $yarrDir/build
